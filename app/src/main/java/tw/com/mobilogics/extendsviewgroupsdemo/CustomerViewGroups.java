3d158e25a6271bf8e42078f184aa30a0d0d5a70e
didi <didi@mobilogics.com.tw>
2014-11-06 17:46:04 +0800

    test result && explain

--- app/src/main/java/tw/com/mobilogics/extendsviewgroupsdemo/CustomerViewGroups.java
+++ app/src/main/java/tw/com/mobilogics/extendsviewgroupsdemo/CustomerViewGroups.java
@@ -1,9 +1,14 @@
 package tw.com.mobilogics.extendsviewgroupsdemo;
 
+import org.w3c.dom.Text;
+
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by xuyouren on 14/11/6.
@@ -11,47 +16,63 @@ import android.view.ViewGroup;
 
 public class CustomerViewGroups extends ViewGroup {
   private final String TAG = CustomerViewGroups.class.getSimpleName();
-  private final int LOG_D = 0;
-  private final int LOG_I = 1;
-  private final int LOG_E = 2;
 
-  private int mMeasureHeight, mHeight;
-  private int mMeasureWidth, mWidth;
+  private View mView;
+  private int mIndex;
 
   public CustomerViewGroups(Context context, AttributeSet attrs) {
     super(context, attrs);
+    mIndex = 0;
   }
 
   @Override
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    mMeasureHeight = getMeasuredHeight();
-    mHeight = getHeight();
-    mMeasureWidth = getMeasuredWidth();
-    mWidth = getWidth();
+
   }
 
   @Override
   protected void onLayout(boolean changed, int l, int t, int r, int b) {
-    LogPrintln(LOG_E, "mH = " + mMeasureHeight);
-    LogPrintln(LOG_E, "H = " + mHeight);
-    LogPrintln(LOG_E, "mW = " + mMeasureWidth);
-    LogPrintln(LOG_E, "W = " + mWidth);
+    mView = getChildAt(0);
+    mView.measure(0,0);
+
+    Log.e(TAG, "width = " + mView.getWidth());
+    Log.e(TAG, "mwidth = " + mView.getMeasuredWidth());
+
+    Log.e(TAG, "height = " + mView.getHeight());
+    Log.e(TAG, "mheight = " + mView.getMeasuredHeight());
+
+    ((TextView)mView).setTextSize(180);
+     mView.layout(l, t, r, b);
+
+    // Do nothing, is only call measure Method to calculate View Height, Width
+    mView.measure(0,0);
   }
 
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
 
-  void LogPrintln(int level, String msg) {
-    switch (level) {
-      case LOG_D :
-        Log.d(TAG, msg);
-        break;
-      case LOG_E :
-        Log.e(TAG, msg);
-        break;
-      case LOG_I :
-        Log.i(TAG, msg);
-        break;
-    }
+//    Log.e(TAG, "width = " + mView.getWidth());
+//    Log.e(TAG, "mwidth = " + mView.getMeasuredWidth());
+//
+//    Log.e(TAG, "height = " + mView.getHeight());
+//    Log.e(TAG, "mheight = " + mView.getMeasuredHeight());
+
+    /**
+     * 總結
+     * getWidth, getHeight
+     *    得到的是view在父Layout中布局好后的宽度值，如果没有父布局，那么默认的父布局就是真个屏幕。
+     *    根據 layout(l,t,r,b) 去調整在父Layout 位置
+     *
+     * getMeasuredWidth, getMeasureHeight
+     *    得到的是某个View想要在parent view里面占的大小
+     *
+     *    Log result
+     *    width 0 => 900
+     *    mwidth 222 => 2859
+     *    height 0 => 450
+     *    mheight 57 = > 718
+     */
   }
-
 }
